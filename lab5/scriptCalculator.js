e54d1e354cc1a1bca217a06315cfbdd5e36f1f2a
IvanOnukov <91004543+IvanOnukov@users.noreply.github.com>
2021-10-10 20:40:46 +0300

    add cheks input data

--- lab5/scriptCalculator.js
+++ lab5/scriptCalculator.js
@@ -1,16 +1,23 @@
-function sumPurchase(){
-    let x = document.getElementById("products").value;
-    let y = document.getElementById("cost").value;
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function sumPurchase() {
+    let products = document.getElementById("products").value;
+    let cost = document.getElementById("cost").value;
     let result;
-    if(x == ''){
-        result = "введите цену товара"; 
+
+    if (!(isNumber(products))) {
+        result = "Допускаются только цифры";
+    } else if (!(isNumber(cost))) {
+        result = "Допускаются только цифры";
+    } else if (products == '') {
+        result = "введите цену товара";
+    } else if (cost == '') {
+        result = "введите количество товара";
+    } else {
+        result = Number(products) * Number(cost);
     }
-    else if(y == ''){
-        result = "введите количество товара"; 
-    }
-    else{
-       result = x*y;  
-    }
-    
+
     document.getElementById("outResult").innerHTML = result;
 }
